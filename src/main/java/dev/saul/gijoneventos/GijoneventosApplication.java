6aad09bbe406d718131e455bb03f3236b48cc70b
saulOMgit <sauloteromelchor@gmail.com>
2025-10-06 12:18:07 +0200

    primer commit

---
+++ src/main/java/dev/saul/gijoneventos/GijoneventosApplication.java
@@ -0,0 +1,13 @@
+package dev.saul.gijoneventos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GijoneventosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GijoneventosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/saul/gijoneventos/GijoneventosApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saul.gijoneventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GijoneventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
