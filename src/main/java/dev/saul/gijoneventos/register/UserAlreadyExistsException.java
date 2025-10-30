2469b538f0c0b6b6c52c8c93569d88e78c1e5f73
Saul <sauloteromelchor@gmail.com>
2025-10-30 11:53:06 +0100

    fix naming

--- src/main/java/dev/saul/gijoneventos/register/UserAlreadyExistsException.java
+++ src/main/java/dev/saul/gijoneventos/register/UserAlreadyExistsException.java
@@ -2,6 +2,6 @@ package dev.saul.gijoneventos.register;
 
 public class UserAlreadyExistsException extends RuntimeException {
     public UserAlreadyExistsException(String username) {
-        super("El usuario con DNI '" + username + "' ya existe.");
+        super("El usuario '" + username + "' ya existe.");
     }
 }
